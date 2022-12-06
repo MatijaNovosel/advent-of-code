fda8bdf473299147313276873b2d4920946ca0d8
Matija Novosel <mnovosel5@gmail.com>
2022-12-06 10:21:07 +0100

    Shortened code

--- 06 - Tuning Trouble/index.js
+++ 06 - Tuning Trouble/index.js
@@ -1,17 +1,12 @@
-import { allUnique, parseLines } from "../utils/index.js";
+import { parseLines } from "../utils/index.js";
 
 const input = parseLines("../06 - Tuning Trouble/input.txt", true)[0];
 
 const detectMarker = (n = 4) => {
-  const stack = [];
   for (let i = 0; i < input.length; i++) {
-    stack.push(input[i]);
-    if (stack.length === n) {
-      if (allUnique(...stack)) {
-        console.log(i + 1);
-        break;
-      }
-      stack.shift();
+    if (new Set(input.substring(i, i + n)).size === n) {
+      console.log(i + n);
+      break;
     }
   }
 };
--- utils/index.js
+++ utils/index.js
@@ -49,7 +49,3 @@ export const intersect = (a, b) => {
   const setB = new Set(b);
   return [...new Set(a)].filter((x) => setB.has(x));
 };
-
-export const allUnique = (...args) => {
-  return [...new Set(args)].toString() === args.toString();
-};
