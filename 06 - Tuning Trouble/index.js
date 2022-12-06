f46a08206bdbe5d0db0fec4e64ec8f8703aea6e6
Matija Novosel <mnovosel5@gmail.com>
2022-12-06 09:49:38 +0100

    Added README.md for every day for further descriptions

---
+++ 06 - Tuning Trouble/index.js
@@ -0,0 +1,5 @@
+import { parseLines } from "../utils/index.js";
+
+const lines = parseLines("../06 - Tuning Trouble/input.txt", true);
+
+console.log(lines);
--- utils/index.js
+++ utils/index.js
@@ -20,6 +20,7 @@ export const splitByValue = (arr, val) => {
 
 export const parseLines = (fileName, trim) => {
   const contents = fs.readFileSync(fileName).toString().split("\n");
+  contents.pop();
 
   if (trim) {
     return contents.map((x) => x.trim());
