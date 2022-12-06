8eca021f4922ffd5b18a1700bd93c6e658501575
Matija Novosel <mnovosel5@gmail.com>
2022-12-06 10:12:23 +0100

    Day 6 - Part 2

--- 06 - Tuning Trouble/index.js
+++ 06 - Tuning Trouble/index.js
@@ -2,19 +2,22 @@ import { allUnique, parseLines } from "../utils/index.js";
 
 const input = parseLines("../06 - Tuning Trouble/input.txt", true)[0];
 
-// Part 1
-
-const stack = [];
-
-for (let i = 0; i < input.length; i++) {
-  stack.push(input[i]);
-  if (stack.length === 4) {
-    if (allUnique(...stack)) {
-      console.log(i + 1);
-      break;
+const detectMarker = (n = 4) => {
+  const stack = [];
+  for (let i = 0; i < input.length; i++) {
+    stack.push(input[i]);
+    if (stack.length === n) {
+      if (allUnique(...stack)) {
+        console.log(i + 1);
+        break;
+      }
+      stack.shift();
     }
-    stack.shift();
   }
-}
+};
+
+// Part 1
+detectMarker();
 
 // Part 2
+detectMarker(14);
