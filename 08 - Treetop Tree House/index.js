2a3803988653d04eb4ca81989f504f28c501f2bf
Matija Novosel <mnovosel5@gmail.com>
2022-12-08 13:40:03 +0100

    Day 8 - Part 2

--- 08 - Treetop Tree House/index.js
+++ 08 - Treetop Tree House/index.js
@@ -1,61 +1,70 @@
 import { parseLines } from "../utils/index.js";
 
-const lines = parseLines("../08 - Treetop Tree House/test.txt", true);
-
-const matrix = [...lines.map((l) => l.split("").map((n) => +n))];
-
-// Part 1
+const lines = parseLines("../08 - Treetop Tree House/input.txt", true);
+let biggestScenicScore = 0;
 let innerTrees = 0;
-const edgeTrees = matrix.length * 2 + (matrix.length - 2) * 2;
+const edgeTrees = lines.length * 2 + (lines.length - 2) * 2;
 
-const visible = (x, y, direction) => {
-  const val = matrix[x][y];
-  let isVisible = true;
+const discover = (x, y) => {
+  const val = lines[x][y];
+  let visibility = 0b1111;
+  let sct = 0;
+  let scr = 0;
+  let scb = 0;
+  let scl = 0;
 
-  switch (direction) {
-    case "t":
-      for (let i = x - 1; i >= 0; i--)
-        if (matrix[i][y] >= val) {
-          isVisible = false;
-          break;
-        }
+  for (let i = x - 1; i >= 0; i--) {
+    if (lines[i][y] >= val) {
+      visibility = visibility & 0b0111;
+      sct++;
       break;
-    case "r":
-      for (let i = y + 1; i < matrix[x].length; i++)
-        if (matrix[x][i] >= val) {
-          isVisible = false;
-          break;
-        }
-      break;
-    case "b":
-      for (let i = x + 1; i < matrix.length; i++)
-        if (matrix[i][y] >= val) {
-          isVisible = false;
-          break;
-        }
-      break;
-    case "l":
-      for (let i = 0; i < y; i++)
-        if (matrix[x][i] >= val) {
-          isVisible = false;
-          break;
-        }
-      break;
+    }
+    sct++;
   }
 
-  return isVisible;
+  for (let i = y + 1; i < lines[x].length; i++) {
+    if (lines[x][i] >= val) {
+      visibility = visibility & 0b1011;
+      scr++;
+      break;
+    }
+    scr++;
+  }
+
+  for (let i = x + 1; i < lines.length; i++) {
+    if (lines[i][y] >= val) {
+      visibility = visibility & 0b1101;
+      scb++;
+      break;
+    }
+    scb++;
+  }
+
+  for (let i = y - 1; i >= 0; i--) {
+    if (lines[x][i] >= val) {
+      visibility = visibility & 0b1110;
+      scl++;
+      break;
+    }
+    scl++;
+  }
+
+  return {
+    score: sct * scr * scb * scl,
+    visible: (visibility ^ 0b1111) !== 0b1111
+  };
 };
 
-for (let x = 1; x < matrix.length - 1; x++) {
-  for (let y = 1; y < matrix[x].length - 1; y++) {
-    const t = visible(x, y, "t");
-    const r = visible(x, y, "r");
-    const b = visible(x, y, "b");
-    const l = visible(x, y, "l");
-    innerTrees += !!(t || r || b || l);
+for (let x = 1; x < lines.length - 1; x++) {
+  for (let y = 1; y < lines[x].length - 1; y++) {
+    const { visible, score } = discover(x, y);
+    if (score > biggestScenicScore) biggestScenicScore = score;
+    innerTrees += !!visible;
   }
 }
 
+// Part 1
 console.log(innerTrees + edgeTrees);
 
 // Part 2
+console.log(biggestScenicScore);
