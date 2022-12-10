9654f338b077cffa0800bcd2ac3d89eabe8079df
Matija Novosel <mnovosel5@gmail.com>
2022-12-10 17:34:35 +0100

    Day 10 wip

--- 10 - Cathode-Ray Tube/index.js
+++ 10 - Cathode-Ray Tube/index.js
@@ -1,35 +1,44 @@
+import { chunk } from "matija-utils";
 import { parseLines } from "../utils/index.js";
 
 const lines = parseLines("../10 - Cathode-Ray Tube/input.txt", true);
 
 let cycle = 0;
-let total = 1;
+let position = 1;
 const history = [];
 
 const sumOfCycleValues = (...args) => {
   let sum = 0;
   args.forEach((n) => {
-    const { cycle, total } = history.filter((h) => h.cycle === n)[0];
-    sum += cycle * total;
+    const { cycle, position } = history.filter((h) => h.cycle === n)[0];
+    sum += cycle * position;
   });
   return sum;
+};
+
+const updateHistory = ({ cycle, position }) => {
+  if (!history.some((h) => h.cycle === cycle))
+    history.push({ cycle, position });
 };
 
 lines.forEach((l) => {
   if (l.startsWith("addx ")) {
     const amount = +l.split(" ")[1];
-    history.push({ cycle, total });
+    updateHistory({ cycle, position });
     cycle++;
-    history.push({ cycle, total });
+    updateHistory({ cycle, position });
     cycle++;
-    history.push({ cycle, total });
-    total += amount;
-    history.push({ cycle, total });
+    updateHistory({ cycle, position });
+    position += amount;
   } else {
     cycle++;
-    history.push({ cycle, total });
   }
+  updateHistory({ cycle, position });
 });
 
 // Part 1
 console.log(sumOfCycleValues(20, 60, 100, 140, 180, 220));
+
+const chunked = chunk(history, 40);
+
+console.log(chunked);
