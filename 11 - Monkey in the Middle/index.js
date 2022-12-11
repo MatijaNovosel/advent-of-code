680b3df96fa01562775bdb29932e5eb6c120506c
Matija Novosel <mnovosel5@gmail.com>
2022-12-11 16:49:09 +0100

    Day 11 - Part 2

--- 11 - Monkey in the Middle/index.js
+++ 11 - Monkey in the Middle/index.js
@@ -1,4 +1,4 @@
-import { chunk, repeat } from "matija-utils";
+import { chunk } from "matija-utils";
 import { parseLines } from "../utils/index.js";
 
 const lines = chunk(
@@ -9,37 +9,55 @@ const lines = chunk(
 ).map((l) => l.slice(1, l.length));
 
 const monkeys = [];
+const initialItems = [];
 
 lines.forEach((l) => {
+  const mod = parseInt(l[2].replace(/[^0-9]/g, ""));
+  const items = l[0]
+    .slice(l[0].indexOf(": ") + 1, l[0].length)
+    .split(", ")
+    .map((n) => +n);
+  initialItems.push([...items]);
   monkeys.push({
-    items: l[0]
-      .slice(l[0].indexOf(": ") + 1, l[0].length)
-      .split(", ")
-      .map((n) => +n),
+    items,
     operation: Function(
       "old",
       `return ${l[1].slice(l[1].indexOf("= ") + 2, l[1].length)}`
     ),
-    test: Function("n", `return n % ${parseInt(l[2].replace(/[^0-9]/g, ""))}`),
-    throwToTrue: parseInt(l[3].replace(/[^0-9]/g, "")),
-    throwToFalse: parseInt(l[4].replace(/[^0-9]/g, "")),
+    mod,
+    throwTo: Function(
+      "n",
+      `return n % ${mod} ? ${parseInt(
+        l[4].replace(/[^0-9]/g, "")
+      )} : ${parseInt(l[3].replace(/[^0-9]/g, ""))}`
+    ),
     inspections: 0
   });
 });
 
-repeat(20, () =>
-  monkeys.forEach((m) => {
-    m.items.forEach((item) => {
-      const worryLevel = Math.floor(m.operation(item) / 3);
-      if (m.test(worryLevel) === 0)
-        monkeys[m.throwToTrue].items.push(worryLevel);
-      else monkeys[m.throwToFalse].items.push(worryLevel);
-      m.items = m.items.filter((i) => i !== item);
-      m.inspections++;
+const superModulo = monkeys.reduce((acc, { mod }) => acc * mod, 1);
+
+const monkeyBusiness = (monkeys, rounds, divisor) => {
+  while (rounds--) {
+    monkeys.forEach((m) => {
+      m.inspections += m.items.length;
+      m.items
+        .map((item) => Math.trunc(m.operation(item) / divisor) % superModulo)
+        .forEach((level) => monkeys[m.throwTo(level)].items.push(level));
+      m.items = [];
     });
-  })
-);
+  }
+  const inspections = monkeys.map((m) => m.inspections).sort((a, b) => b - a);
+  return inspections[0] * inspections[1];
+};
 
 // Part 1
-monkeys.sort((a, b) => b.inspections - a.inspections);
-console.log(monkeys[0].inspections * monkeys[1].inspections);
+console.log(monkeyBusiness(monkeys, 20, 3));
+
+for (let i = 0; i < monkeys.length; i++) {
+  monkeys[i].items = initialItems[i];
+  monkeys[i].inspections = 0;
+}
+
+// Part 2
+console.log(monkeyBusiness(monkeys, 10000, 1));
