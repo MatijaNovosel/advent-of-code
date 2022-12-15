91706e59318ee91fd550af0909f08b34310bef1b
Matija Novosel <mnovosel5@gmail.com>
2022-12-15 14:14:54 +0100

    Day 15 - Part 2 wip

--- 15 - Beacon Exclusion Zone/README.md
+++ 15 - Beacon Exclusion Zone/README.md
@@ -108,3 +108,11 @@ In this example, in the row where `y=10`, there are `26` positions where a beaco
 Consult the report from the sensors you just deployed. **In the row where y=2000000, how many positions cannot contain a beacon?**
 
 <h1 align=center>Part two</h1>
+
+Your handheld device indicates that the distress signal is coming from a beacon nearby. The distress beacon is not detected by any sensor, but the distress beacon must have `x` and `y` coordinates each no lower than `0` and no larger than `4000000`.
+
+To isolate the distress beacon's signal, you need to determine its **tuning frequency**, which can be found by multiplying its `x` coordinate by `4000000` and then adding its `y` coordinate.
+
+In the example above, the search space is smaller: instead, the `x` and `y` coordinates can each be at most `20`. With this reduced search area, there is only a single position that could have a beacon: `x=14, y=11`. The tuning frequency for this distress beacon is `56000011`.
+
+Find the only possible position for the distress beacon. **What is its tuning frequency?**
--- 15 - Beacon Exclusion Zone/index.js
+++ 15 - Beacon Exclusion Zone/index.js
@@ -1,3 +1,65 @@
+import { difference, end, range } from "matija-utils";
 import { parseLines } from "../utils/index.js";
 
-const lines = parseLines("../15 - Beacon Exclusion Zone/test.txt", true);
+const coordinates = parseLines(
+  "../15 - Beacon Exclusion Zone/input.txt",
+  true
+).map((l) => {
+  return l
+    .match(
+      /^Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)$/
+    )
+    .slice(1, 5)
+    .map(Number);
+});
+
+const calc = (offset = 2_000_000) => {
+  const intervals = [];
+  const visited = new Set();
+  for (const [sx, sy, bx, by] of coordinates) {
+    const diff = Math.abs(sx - bx) + Math.abs(sy - by) - Math.abs(sy - offset);
+    if (diff < 0) continue;
+    intervals.push({ x: sx - diff, y: sx + diff });
+    if (by === offset) visited.add(bx);
+  }
+  intervals.sort();
+  const arr = [];
+  for (const { x, y } of intervals) {
+    if (!arr.length) {
+      arr.push({ x, y });
+      continue;
+    }
+    const { y: endY } = end(arr, 1);
+    if (x > endY + 1) {
+      arr.push({ x, y });
+      continue;
+    }
+    end(arr, 1).y = Math.max(endY, y);
+  }
+  return { intervals, visited, arr };
+};
+
+// Part 1
+const { intervals, visited } = calc();
+const forbidden = new Set();
+for (const { x, y } of intervals) range(x, y).forEach((n) => forbidden.add(n));
+console.log(difference(forbidden, visited).length);
+
+// Part 2
+let found = false;
+const max = 4_000_000;
+
+for (const n of range(0, max)) {
+  if (found) break;
+  const { arr } = calc(n);
+  let res = 0;
+  for (const { x, y } of arr) {
+    if (res < x) {
+      console.log(res * max + n);
+      found = true;
+      break;
+    }
+    res = Math.max(res, y + 1);
+    if (res > max) break;
+  }
+}
