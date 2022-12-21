7c9c24ac63655cfe20077e9a498be8624ccfc7af
Matija Novosel <mnovosel5@gmail.com>
2022-12-21 23:28:08 +0100

    Day 20, Day 21 part 1

--- 20 - Grove Positioning System/README.md
+++ 20 - Grove Positioning System/README.md
@@ -59,3 +59,52 @@ Initial arrangement:
 Then, the grove coordinates can be found by looking at the 1000th, 2000th, and 3000th numbers after the value 0, wrapping around the list as necessary. In the above example, the 1000th number after `0` is `4`, the 2000th is `-3`, and the 3000th is `2`; adding these together produces `3`.
 
 Mix your encrypted file exactly once. **What is the sum of the three numbers that form the grove coordinates?**
+
+<h1 align=center>Part two</h1>
+
+The grove coordinate values seem nonsensical. While you ponder the mysteries of Elf encryption, you suddenly remember the rest of the decryption routine you overheard back at camp.
+
+First, you need to apply the **decryption key**, `811589153`. Multiply each number by the decryption key before you begin; this will produce the actual list of numbers to mix.
+
+Second, you need to mix the list of numbers **ten times**. The order in which the numbers are mixed does not change during mixing; the numbers are still moved in the order they appeared in the original, pre-mixed list. (So, if -3 appears fourth in the original list of numbers to mix, -3 will be the fourth number to move during each round of mixing.)
+
+Using the same example as above:
+
+```
+Initial arrangement:
+811589153, 1623178306, -2434767459, 2434767459, -1623178306, 0, 3246356612
+
+After 1 round of mixing:
+0, -2434767459, 3246356612, -1623178306, 2434767459, 1623178306, 811589153
+
+After 2 rounds of mixing:
+0, 2434767459, 1623178306, 3246356612, -2434767459, -1623178306, 811589153
+
+After 3 rounds of mixing:
+0, 811589153, 2434767459, 3246356612, 1623178306, -1623178306, -2434767459
+
+After 4 rounds of mixing:
+0, 1623178306, -2434767459, 811589153, 2434767459, 3246356612, -1623178306
+
+After 5 rounds of mixing:
+0, 811589153, -1623178306, 1623178306, -2434767459, 3246356612, 2434767459
+
+After 6 rounds of mixing:
+0, 811589153, -1623178306, 3246356612, -2434767459, 1623178306, 2434767459
+
+After 7 rounds of mixing:
+0, -2434767459, 2434767459, 1623178306, -1623178306, 811589153, 3246356612
+
+After 8 rounds of mixing:
+0, 1623178306, 3246356612, 811589153, -2434767459, 2434767459, -1623178306
+
+After 9 rounds of mixing:
+0, 811589153, 1623178306, -2434767459, 3246356612, 2434767459, -1623178306
+
+After 10 rounds of mixing:
+0, -2434767459, 1623178306, 3246356612, -1623178306, 2434767459, 811589153
+```
+
+The grove coordinates can still be found in the same way. Here, the 1000th number after `0` is `811589153`, the 2000th is `2434767459`, and the 3000th is `-1623178306`; adding these together produces `1623178306`.
+
+Apply the decryption key and mix your encrypted file ten times. **What is the sum of the three numbers that form the grove coordinates?**
--- 20 - Grove Positioning System/index.js
+++ 20 - Grove Positioning System/index.js
@@ -1,32 +1,88 @@
+import { mod } from "matija-utils";
 import { parseLines } from "../utils/index.js";
 
-const numbers = parseLines(
-  "../20 - Grove Positioning System/test.txt",
-  true
-).map(Number);
-
-const move = (idx, moves, arr) => {
-  const len = arr.length;
-  let newIdx = idx;
-  const toMove = Math.abs(moves) % len;
-  if (moves > 0) {
-    if (idx + toMove < len) newIdx = idx + toMove;
-    else if (idx + toMove > len) newIdx = toMove - idx - 2;
-  } else if (moves < 0) {
-    if (idx - toMove > 0) newIdx = idx - toMove;
-    else if (idx - toMove < 0) newIdx = len - toMove;
-  }
-  const [a, b] = [arr[newIdx], arr[idx]];
-  arr[newIdx] = b;
-  arr[idx] = a;
+const equal = (a, b) => {
+  if (a === null || b === null) return false;
+  return a.n === b.n && a.r.n === b.r.n && a.l.n === b.l.n;
 };
 
-for (let i = 0; i < base.length; i++) {
-  console.log({
-    numbers,
-    i,
-    number: numbers[i]
-  });
-  move(i, numbers[i], numbers);
-  console.log({ numbers });
+const nodes = parseLines("./20 - Grove Positioning System/input.txt", true).map(
+  (n) => ({ l: null, n: +n, r: null })
+);
+
+const nodes2 = JSON.parse(JSON.stringify(nodes)).map((n) => {
+  n.n *= 811589153;
+  return n;
+});
+
+for (let i = 0; i < nodes.length; i++) {
+  nodes[i].r = nodes[mod(i + 1, nodes.length)];
+  nodes[i].l = nodes[mod(i - 1, nodes.length)];
+  nodes2[i].r = nodes2[mod(i + 1, nodes2.length)];
+  nodes2[i].l = nodes2[mod(i - 1, nodes2.length)];
 }
+
+const parseNodes = (nodes, acc, iteratee) => {
+  for (const k of nodes) {
+    if (k.n === 0) {
+      acc = k;
+      continue;
+    }
+    iteratee = k;
+    if (k.n > 0) {
+      for (let i = 0; i < mod(k.n, nodes.length - 1); i++)
+        iteratee = iteratee.r;
+      if (equal(k, iteratee)) continue;
+      k.r.l = k.l;
+      k.l.r = k.r;
+      iteratee.r.l = k;
+      k.r = iteratee.r;
+      iteratee.r = k;
+      k.l = iteratee;
+    } else {
+      for (let i = 0; i < mod(-k.n, nodes.length - 1); i++)
+        iteratee = iteratee.l;
+      if (equal(k, iteratee)) continue;
+      k.l.r = k.r;
+      k.r.l = k.l;
+      iteratee.l.r = k;
+      k.l = iteratee.l;
+      iteratee.l = k;
+      k.r = iteratee;
+    }
+  }
+  return { accNew: acc, iterateeNew: iteratee };
+};
+
+// Part 1
+let ctr = 0;
+let acc = null;
+let iteratee = null;
+
+const { accNew } = parseNodes(nodes, acc, iteratee);
+acc = accNew;
+
+for (let i = 0; i < 3; i++) {
+  for (let j = 0; j < 1000; j++) acc = acc.r;
+  ctr += acc.n;
+}
+
+console.log(ctr);
+
+// Part 2
+ctr = 0;
+acc = null;
+iteratee = null;
+
+for (let i = 0; i < 10; i++) {
+  const { accNew, iterateeNew } = parseNodes(nodes2, acc, iteratee);
+  acc = accNew;
+  iteratee = iterateeNew;
+}
+
+for (let i = 0; i < 3; i++) {
+  for (let j = 0; j < 1000; j++) acc = acc.r;
+  ctr += acc.n;
+}
+
+console.log(ctr);
